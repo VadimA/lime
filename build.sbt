ca77e2a838901e6acb6ac7dce79d2b52169411f2
VadimA <wadimanosov@gmail.com>
2018-04-03 18:36:07 +0400

    Added 4 resolved problems(6-9) and unit tests for all exercises

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "limesbt"
+
+version := "1.0"
+
+scalaVersion := "2.12.5"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
---
+++ src/main/scala/problems/NinetyNine.scala
@@ -0,0 +1,99 @@
+package main.scala.problems
+
+import java.util
+
+import scala.collection.immutable
+
+/**
+  * Created by vanosov on 02.04.2018.
+  */
+object NinetyNine{
+
+  //P01 (*) Find the last element of a list.
+  def last(list: List[Int]): Int ={
+    if(list.tail.isEmpty)
+      list.head
+    else
+      last(list.tail)
+  }
+
+  //P02 (*) Find the last but one element of a list.
+  def prelast(list: List[Int]): Int ={
+    if(list.tail.size == 1)
+      list.head
+    else
+      prelast(list.tail)
+  }
+
+  //P03 (*) Find the Kth element of a list.
+  def nth(index:Int, list: List[Int]): Int = {
+    if (list.isEmpty)
+      -1
+    else if (index == 0)
+      list.head
+    else
+      nth(index - 1, list.tail)
+  }
+
+  //Find the number of elements of a list.
+  def length[A](list: List[A]): Int = {
+    if(list.isEmpty)
+      0
+    else
+      1 + length(list.tail)
+  }
+
+  //P05 (*) Reverse a list.
+  def reverse(list: List[Int]): List[Int] = {
+    if(list.isEmpty)
+      Nil
+    else
+      reverse(list.tail) :+ list.head
+  }
+
+  //P06 (*) Find out whether a list is a palindrome.
+  def isPalindrome(list: List[Int]): Boolean = {
+    if(list.equals(reverse(list)))
+      true
+    else
+      false
+  }
+
+  //P07 (**) Flatten a nested list structure.
+  def flatten(list: List[Any]): List[Any] = list flatMap {
+    case ms: List[_] => flatten(ms)
+    case e => List(e)
+  }
+
+  //P08 (**) Eliminate consecutive duplicates of list elements.
+  //If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+  def compress(list: List[Any]): List[Any] = {
+    if(list.isEmpty)
+      Nil
+    else if(!list.tail.isEmpty && (list.head == list.tail.head)){
+      compress(list.tail)
+    }
+    else
+      list.head +: compress(list.tail)
+  }
+
+  //P09 (**) Pack consecutive duplicates of list elements into sublists.
+  //If a list contains repeated elements they should be placed in separate sublists.
+  def pack(list: List[Any]): List[List[Any]] = {
+    if(list.isEmpty)
+      Nil
+    List()
+}
+
+  def main(args:Array[String]): Unit = {
+    println(last(List(1,2,3,1,5,13,567)));
+    println(prelast(List(1,2,3,1,5,13,567)));
+    println(nth(6,List(1,2,3,1,5,13,567)));
+    println(length(List("12","324","1")));
+    println(reverse(List(1,2,3,1,5,13,567)));
+    println(isPalindrome(List(1,2,3,4,3,2,1)));
+    println(flatten(List(List(1, 1), 2, List(3, List(5, 8)))));
+    println(compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)));
+    println(pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)));
+  }
+}
